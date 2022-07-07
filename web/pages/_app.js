2b65ac5294b3c6ee52dad7d3e88af02c3ec19f55
huydq-2329 <dao.quang.huy@sun-asterisk.com>
2022-07-07 14:10:21 +0700

    feat: add context authentication

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import App from 'next/app';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.css'
+import AuthContext from './contexts/AuthContext';
+
+NextApp.getInitialProps = async (ctx) => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
+  const user = await response.json();
+  const appData = App.getInitialProps(ctx);
+
+  return {
+    ...appData,
+    user,
+  }
 }
 
-export default MyApp
+function NextApp({ Component, pageProps, user }) {
+  return (
+    <AuthContext.Provider value={user}>
+      <Component {...pageProps} />
+    </AuthContext.Provider>
+  )
+}
+
+export default NextApp;
